9327373a463800beb6b0f60e89254c5fe4a76470
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-07-29 14:36:08 +0200

    card choices

--- src/App.js
+++ src/App.js
@@ -14,8 +14,10 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
   
-
+  // shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
@@ -25,7 +27,11 @@ function App() {
     setTurns(0);
   };
 
-  console.log(cards, turns);
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+  
 
   return (
     <div className="App">
@@ -34,7 +40,11 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard 
+            key={card.id} 
+            card={card}
+            handleChoice={handleChoice} 
+          />
         ))}
       </div>
     </div>
